4c812a01405ac0c9b09b288864a839bb3e94b7b8
Marko de Swardt <u24658562@tuks.co.za>
2026-08-31 19:08:33 +0300

    Created skeleton cpp file for the PinballAlley.cpp file

---
+++ practicals/practical_3/PinballAlley.cpp
@@ -0,0 +1,41 @@
+// Dian le Roux (25147065)
+// Marko de Swardt (24658562)
+// Jay Macaskill (25198387)
+
+// COS 214 (Software Modelling) Practical 3
+// Last Modified: 31 August 2026
+
+// PinballAlley.cpp
+
+#ifndef PINBALLALLEY_CPP
+#define PINBALLALLEY_CPP
+
+#include "PinballAlley.h"
+#include "EventUnit.h"
+#include "Notice.h"
+
+#include <iostream>
+
+using namespace std;
+
+PinballAlley::PinballAlley(const string& name, int capacity) : EventUnit(name, capacity)
+{
+
+}
+
+void PinballAlley::open()
+{
+	
+}
+
+void PinballAlley::close()
+{
+	
+}
+
+void PinballAlley::update(Notice& notice)
+{
+	
+}
+
+#endif //  PINBALLALLEY_CPP
